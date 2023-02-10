355129f6c3f404db612bec192b37a4470d818fc4
Eroshchev Semyon <semeneroshev@gmail.com>
2023-02-10 23:19:37 +0300

    Add:
    - TargetCondition
    - Node Entity
    - Switching from empty node to building by click

--- Graph/Assets/Ruinum.ECS/Core/Configurations/Conditions/AnyCondition.cs
+++ Graph/Assets/Ruinum.ECS/Core/Configurations/Conditions/AnyCondition.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Ruinum.ECS.Core.Conditions
---
+++ Graph/Assets/Ruinum.ECS/Core/Configurations/Conditions/TargetCondition.cs
@@ -0,0 +1,18 @@
+﻿using Ruinum.ECS.Configurations.Game.Strategies.Targets;
+
+namespace Ruinum.ECS.Core.Conditions
+{
+    public class TargetCondition : EntityCondition
+    {
+        public ITargetStrategy TargetStrategy;
+        public IEntityCondition Condition;
+        protected override bool IsTrue(GameEntity entity)
+        {
+            if (!TargetStrategy.TryGet(entity, out var target))
+            {
+                return false;
+            }
+            return Condition.IsConditionTrue(target);
+        }
+    }
+}
