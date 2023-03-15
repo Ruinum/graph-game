bb13361b151b3c27a5ca662c474b0104d721b672
Illusion <66023494+Mimist-Illusionard@users.noreply.github.com>
2023-03-15 22:10:21 +0300

    Change:
    - Unit capture logic (in work)

---
+++ Graph/Assets/Ruinum.ECS/Core/Scripts/Entities/Components/Game/CreateEntityNestedBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using Ruinum.ECS.Configurations.Game;
+
+namespace Ruinum.ECS.Scripts.Entities.Components.Game
+{
+    public sealed class CreateEntityNestedBehaviour : GameEntityComponentBehaviour
+    {
+        public GameEntityConfig EntityConfig;
+
+        protected override void OnSetEntity(GameEntity entity)
+        {
+            EntityConfig.Create(entity);
+        }
+    }
+}
