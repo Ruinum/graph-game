e72fab2d604166940529a8874e1c357c2ce226a5
Illusion <66023494+Mimist-Illusionard@users.noreply.github.com>
2023-03-18 18:15:26 +0300

    Add:
    - Road connection

--- Graph/Assets/Ruinum.ECS/Core/Configurations/Game/Strategies/Subscribers/Values/PointsDistanceByAxisValueStrategy.cs
+++ Graph/Assets/Ruinum.ECS/Core/Configurations/Game/Strategies/Subscribers/Values/PointsDistanceByAxisValueStrategy.cs
@@ -1,4 +1,4 @@
-﻿//using Ruinum.ECS.Configurations.Game.Strategies.VectorStrategies;
+﻿using Ruinum.ECS.Configurations.Game.Strategies.VectorStrategies;
 using Ruinum.ECS.Constants;
 using Ruinum.ECS.Utilities;
 using Sirenix.OdinInspector;
@@ -8,33 +8,32 @@ namespace Ruinum.ECS.Configurations.Game.Strategies.Subscribers.Values
 {
     public sealed class PointsDistanceByAxisValueStrategy : FloatValueBaseStrategy
     {
-        //[LabelWidth(EditorConstants.SmallLabelWidth)] public VectorStrategy First;
-        //[LabelWidth(EditorConstants.SmallLabelWidth)] public VectorStrategy Second;
-        //[LabelWidth(EditorConstants.SmallLabelWidth)] public Vector3Axis Axis = Vector3Axis.Y;
-        //[LabelWidth(EditorConstants.SmallLabelWidth)] public bool Absolute = true;
+        [LabelWidth(EditorConstants.SmallLabelWidth)] public VectorStrategy First;
+        [LabelWidth(EditorConstants.SmallLabelWidth)] public VectorStrategy Second;
+        [LabelWidth(EditorConstants.SmallLabelWidth)] public Vector3Axis Axis = Vector3Axis.Y;
+        [LabelWidth(EditorConstants.SmallLabelWidth)] public bool Absolute = true;
 
         public override bool TryGet(GameEntity entity, out float value)
         {
-            value = default;
-            //if (!First.TryGet(entity, out var firstPoint))
-            //{
-            //    value = default;
-            //    if(Logging) LogErrorNotFound(nameof(firstPoint), (nameof(entity), entity));
-            //    return false;
-            //}
+            if (!First.TryGet(entity, out var firstPoint))
+            {
+                value = default;
+                if (Logging) LogErrorNotFound(nameof(firstPoint), (nameof(entity), entity));
+                return false;
+            }
 
-            //if (!Second.TryGet(entity, out var secondPoint))
-            //{
-            //    value = default;
-            //    if(Logging) LogErrorNotFound(nameof(secondPoint), (nameof(entity), entity), (nameof(firstPoint), firstPoint));
-            //    return false;
-            //}
+            if (!Second.TryGet(entity, out var secondPoint))
+            {
+                value = default;
+                if (Logging) LogErrorNotFound(nameof(secondPoint), (nameof(entity), entity), (nameof(firstPoint), firstPoint));
+                return false;
+            }
 
-            //value = VectorUtilities.GetAxisValue(Axis, firstPoint) - VectorUtilities.GetAxisValue(Axis, secondPoint);
-            //if (Absolute)
-            //{
-            //    value = Mathf.Abs(value);
-            //}
+            value = VectorUtilities.GetAxisValue(Axis, firstPoint) - VectorUtilities.GetAxisValue(Axis, secondPoint);
+            if (Absolute)
+            {
+                value = Mathf.Abs(value);
+            }
             return true;
         }
     }
